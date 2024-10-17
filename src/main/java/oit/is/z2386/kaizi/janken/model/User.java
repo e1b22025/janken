c61b02a01e2a60dacfca126e51d3825cef018db6
e1b22025 <e1b22025@oit.ac.jp>
2024-10-17 11:00:45 +0900

    試合結果をSELECTで取得して表示する

--- src/main/java/oit/is/z2386/kaizi/janken/model/User.java
+++ src/main/java/oit/is/z2386/kaizi/janken/model/User.java
@@ -2,28 +2,25 @@ package oit.is.z2386.kaizi.janken.model;
 
 public class User {
 
-  public class Users {
-    int id;
-    String name;
+  int id;
+  String name;
 
-    // Thymeleafでフィールドを扱うためにはgetter/setterが必ず必要
-    // vscodeのソースコード右クリック->ソースアクションでsetter/getterを簡単に追加できる
-    public String getName() {
-      return name;
-    }
+  // Thymeleafでフィールドを扱うためにはgetter/setterが必ず必要
+  // vscodeのソースコード右クリック->ソースアクションでsetter/getterを簡単に追加できる
+  public String getName() {
+    return name;
+  }
 
-    public void setName(String name) {
-      this.name = name;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public int getId() {
-      return id;
-    }
+  public int getId() {
+    return id;
+  }
 
-    public void setId(int id) {
-      this.id = id;
-    }
-
+  public void setId(int id) {
+    this.id = id;
   }
 
 }
